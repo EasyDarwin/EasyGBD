c0f8e362f9c8ecbbabb2177b60d19704a70f5b60
Babosa <babosa@easydarwin.org>
2025-02-14 16:45:33 +0800

    remove Android old version

--- Android/library/src/main/java/org/easydarwin/util/BUSUtil.java
+++
@@ -1,8 +0,0 @@
-package org.easydarwin.util;
-
-import com.squareup.otto.Bus;
-import com.squareup.otto.ThreadEnforcer;
-
-public class BUSUtil {
-    public static final Bus BUS = new Bus(ThreadEnforcer.ANY);
-}
